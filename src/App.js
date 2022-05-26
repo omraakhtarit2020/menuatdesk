522b3c07d751df8cfb2b3a2fc47888dfe3918c1b
omra akhtar <umraakhtar786@gmail.com>
2022-05-27 01:34:59 +0530

    menu

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+import Menu from "./Menu";
+import Categories from "./Categories"
+import items from "./Data"
+
+const allCategories=['all',...new Set(items.map((item)=>item.category))];//taking out cateogry from the array of object
+
 
 function App() {
+  const [menuItems,setMenuItems]=useState(items)
+  const [categories,setCategories]=useState(allCategories);
+  const filterItems=(category)=>{
+    if(category==='all')
+    {
+      setMenuItems(items)
+      return;
+    }
+    const newItems=items.filter((item)=>item.category===category)
+    setMenuItems(newItems)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+     <div>
+       <h2 className='heading'>Our Menu</h2>
+       <div className='underline'></div>
+     </div>
+     <Categories categories={categories} filterItems={filterItems} />
+     <Menu items={menuItems}/>
+    </main>
   );
 }
 
---
+++ src/Menu.js
@@ -0,0 +1,63 @@
+import React from 'react';
+
+const Menu = ({ items }) => {
+  return (
+    <div className='section-center'>
+      {items.map((menuItem) => {
+        const { id, title, img, desc, price } = menuItem;
+        return (
+          <article key={id} className='menu-item'>
+              <img src={img} alt={title} className='photo' />
+                <div className='item-info'>
+                <header>
+                    <h4>{title}</h4>
+                    <h4 className='price'>${price}</h4>
+                </header>
+                <p className='item-text'>{desc}</p>
+                </div>
+          </article>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Menu;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react'
+
+// const Menu = ({menuItems}) => {
+//   return (
+//       <>
+//         {menuItems.map((items)=>{
+//             const {id,title,img,desc,price}=items
+//             return (<div key={id}>
+//                 <img src={img} alt={title}/>
+//                 <div className='item-info'>
+//                     <header>
+//                             <h4>{title}</h4>
+//                             <h4>{price}</h4>
+//                     </header>
+//                     <p>{desc}</p>
+//                 </div>
+//             </div>
+//             );
+//         })}
+//         </>
+//   )
+// }
+
+// export default Menu
